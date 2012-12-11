9b8c28a565f04b711219fef3c995c67462342209
Mathias Paumgarten <paumgarten@soapcreative.com>
2012-12-11 15:05:57 -0800

    initial commit

---
+++ SmartDuplicateCommand.py
@@ -0,0 +1,30 @@
+import sublime, sublime_plugin, re
+
+class SmartDuplicateCommand( sublime_plugin.TextCommand ):
+
+    test = "mathias.width"
+
+    def run( self, edit ):
+        for region in self.view.sel():
+
+            if region.empty():
+
+                line = self.view.line( region )
+                line_contents = self.scan( '\n' + self.view.substr( line ) )
+
+                self.view.insert( edit, line.end(), line_contents )
+
+            else:
+                self.view.insert( edit, region.begin(), self.view.substr( region ) )
+
+    def scan( self, string ):
+
+        def replace( match ):
+            if ( match.group( 0 ) == ".height" ): return ".width"
+            elif ( match.group( 0 ) == ".width" ): return ".height"
+            elif ( match.group( 0 ) == ".x" ): return ".y"
+            elif ( match.group( 0 ) == ".y" ): return ".x"
+
+        string = re.sub( "(.height|.width|.x|.y)", replace, string )
+
+        return string
